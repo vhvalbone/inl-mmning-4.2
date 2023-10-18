ea11338215cdc572037e464248324bc079af3855
alsv06003 <alsv06003@E-PW0234JW>
2023-10-18 10:36:28 +0200

    Add project files.

---
+++ inlämmning 4.2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace inlämmning_4._2
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("skriv in ett heltal");
+            int heltal=int.Parse(Console.ReadLine());
+            int störst = heltal;
+            string svar = "";
+                while(svar !="n")
+            {
+                Console.WriteLine("skriv in ett till heltal om du vill? j/n");
+                svar = Console.ReadLine();
+                if (svar == "n")
+                {
+                    break;
+                }
+                Console.WriteLine("skriv in ett till heltal");
+                int nyttTal=int.Parse(Console.ReadLine());
+                if (nyttTal > störst) ;
+                {
+                    störst = nyttTal;
+                }
+                
+            }
+            Console.WriteLine("det största talet är " + störst);
+        }
+    }
+}
